73b7b788c377fbe29af4f01bbb70f369079a8353
Zin PHtet <zinpainghtet.215108@gmail.com>
2022-05-24 14:42:25 +0630

    light and dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,22 @@ const squareBtn = selectDom('.square_icon i');
 const closeBtn = selectDom('.close i');
 const nav = selectDom('.nav_bar');
 squareBtn.addEventListener('click', () => {
-	// alert('hello');
 	navBar.classList.add('show');
-	// navBar.style.right = '0';
 });
 closeBtn.addEventListener('click', () => {
-	// alert('hello');
 	navBar.classList.remove('show');
-	// navBar.style.right = '0';
+});
+
+const nav_link = document.querySelectorAll('.nav_link');
+const navLinks = Array.from(nav_link);
+navLinks.forEach(function (link) {
+	link.addEventListener('click', () => {
+		navBar.classList.remove('show');
+	});
 });
 
 window.addEventListener('scroll', function () {
-	console.log(this.scrollY);
+	// console.log(this.scrollY);
 	if (this.scrollY > 100) {
 		nav.classList.add('sticky');
 	} else {
@@ -65,4 +69,38 @@ window.addEventListener('scroll', function () {
 	} else {
 		toHome.style.display = 'none';
 	}
+});
+
+//ark and Dark
+
+const modeIcon = selectDom('.light_dark i');
+const modeText = selectDom('.light_dark .mode_text');
+const body = selectDom('body');
+const imgs = document.querySelectorAll('.company img');
+const companyImgs = Array.from(imgs);
+
+modeIcon.addEventListener('click', function () {
+	if (this.className === 'ri-moon-line') {
+		this.className = 'ri-sun-line';
+		modeText.textContent = 'Light Mode';
+		body.classList.add('dark');
+		companyImgs.forEach((img) => {
+			img.style.filter = 'invert(100%)';
+		});
+
+		return;
+	}
+	this.className = 'ri-moon-line';
+	body.classList.remove('dark');
+	modeText.textContent = 'Dark Mode';
+	companyImgs.forEach((img) => {
+		img.style.filter = 'none';
+	});
+});
+
+//Animate On Scroll
+
+AOS.init({
+	duration: 1200,
+	once: true,
 });
